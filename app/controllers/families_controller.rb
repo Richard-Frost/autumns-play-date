66cefa9da44338cf4dccd07bc533189b210f2bfd
Richard-Frost <frost.rchrd@gmail.com>
2018-10-31 11:56:43 -0400

    added family controller actions

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,7 +1,31 @@
 class FamiliesController < ApplicationController
+  
+
   def index
   end
 
   def new 
+    @family = Family.new
+    @family.children.build
+    @family.parents.build
   end
+
+  def create
+    @family = Family.new(family_params)
+    @family.save
+    redirect_to family_path(@family.id)
+  end
+
+  def show
+    @family = Family.find(params[:id])
+  end
+
+
+
+private
+
+  def family_params
+    params.require(:family).permit(:email, :password, parents_attributes: [:name, :email,:password], children_attributes: [:name, :age])
+  end
+
 end
