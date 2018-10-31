34b6cb847bc90a1052667d6a5eac5c13f537f3f5
Richard-Frost <frost.rchrd@gmail.com>
2018-10-31 11:53:40 -0400

    added family views, sign up form and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :families
+  resources :children
+  resources :parents
+  get '/signup' => 'families#new'
 end
